4f38ed70d2377c116653f03fcaa0818aa7c517a2
Sandeep <sandeepm.mice@gmail.com>
2024-07-24 11:10:49 +0530

    program to count all distinct substrings of the given string

--- src/main/java/StringProblems/DistinctSubStrings.java
+++ src/main/java/StringProblems/DistinctSubStrings.java
@@ -1,2 +1,28 @@
-package StringProblems;public class DistinctSubStrings {
+package StringProblems;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+    Given a string, count all distinct substrings of the given string.
+    Input : abcd
+    Output : abcd abc ab a bcd bc b cd c d
+    All Elements are Distinct
+ */
+public class DistinctSubStrings {
+    public static void main(String[] args) {
+        String str = "srishti";
+        findDistinctSubStrings(str);
+    }
+
+    private static void findDistinctSubStrings(String str) {
+        Set<String> distinctSubstrings = new HashSet<>();
+        for(int i=0;i<=str.length();i++){
+            for(int j=i+1; j<=str.length();j++){
+                distinctSubstrings.add(str.substring(i,j));
+            }
+        }
+        System.out.println("Count :"+ distinctSubstrings.size());
+        distinctSubstrings.forEach(System.out::println);
+    }
 }
